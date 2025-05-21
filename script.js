6c5e47e7b30afbff15006613427caf122e62a2fd
chadriley722 <chadriley722@gmail.com>
2025-05-21 14:26:11 -0600

    Add calculator state variables and operation function

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Calculator state
+let firstNumber = '';      // First number in the operation
+let operator = '';         // The operator (+, -, *, /)
+let secondNumber = '';     // Second number in the operation
+let resetScreen = false;   // Flag to know when to reset the display
+
 // Basic math operations
 function add(a, b) {
     return a + b;
@@ -18,6 +24,38 @@ function divide(a, b) {
     return a / b;
 }
 
+// Perform the calculation based on the operator and numbers
+function operate() {
+    if (!firstNumber || !operator || !secondNumber) return null;
+    
+    const a = parseFloat(firstNumber);
+    const b = parseFloat(secondNumber);
+    
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return null;
+    }
+}
+
+// Test the operation with variables
+function testOperation(num1, op, num2) {
+    firstNumber = num1.toString();
+    operator = op;
+    secondNumber = num2.toString();
+    
+    const result = operate();
+    console.log(`${firstNumber} ${operator} ${secondNumber} = ${result}`);
+    return result;
+}
+
 // Test the functions in the console
 console.log("Testing calculator functions:");
 console.log("2 + 3 =", add(2, 3));        // Expected: 5
@@ -25,5 +63,12 @@ console.log("7 - 4 =", subtract(7, 4));   // Expected: 3
 console.log("5 * 6 =", multiply(5, 6));   // Expected: 30
 console.log("15 / 3 =", divide(15, 3));   // Expected: 5
 
+// Test the operation function
+console.log("\nTesting operation function:");
+testOperation('3', '+', '5');    // Should log: 3 + 5 = 8
+testOperation('10', '-', '7');  // Should log: 10 - 7 = 3
+testOperation('4', '*', '6');    // Should log: 4 * 6 = 24
+testOperation('20', '/', '5');   // Should log: 20 / 5 = 4
+
 // Uncomment to test error case
-// console.log("5 / 0 =", divide(5, 0));  // Will throw an error
+// testOperation('5', '/', '0');  // Will throw an error
